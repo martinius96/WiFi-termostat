c6c487225c44718e90f1d7b18ed982a05e657a0a
Martin Chlebovec <martinius96@gmail.com>
2021-01-01 16:43:20 +0100

    Update Arduino_W5500_JSON_client.ino

--- JSON_client/Arduino_W5500_JSON_client/Arduino_W5500_JSON_client.ino
+++ JSON_client/Arduino_W5500_JSON_client/Arduino_W5500_JSON_client.ino
@@ -1,3 +1,13 @@
+/*|-------------------------------------------------|*/
+/*|Projekt: JSON client - Arduino + Ethernet W5500  |*/
+/*|Popis: Pripojenie k WiFi termostatu v LAN sieti  |*/
+/*|stiahnutie JSON dat, vyparsovanie hodnot, vypis  |*/
+/*|Autor: Martin Chlebovec                          |*/
+/*|E-mail: martinius96@gmail.com                    |*/
+/*|Revízia: 1. Januar 2021                          |*/
+/*|-------------------------------------------------|*/
+//POZNAMKA: NEZABUDNITE PRI FYZICKOM MODULU S ARDUINOM PREPOJENI PRIPOJIT I INT (INTERRUPT) NA D2
+
 #include <Ethernet2.h>
 #include <ArduinoJson.h>
 const char* host = "192.168.1.XX"; //IP adresa WiFi termostatu
