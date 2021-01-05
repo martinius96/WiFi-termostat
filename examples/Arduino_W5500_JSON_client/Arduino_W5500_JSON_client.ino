57e03a83aeb0c72266c0e6004ef3a16b362d8c70
Martin Chlebovec <martinius96@gmail.com>
2021-01-05 17:43:41 +0100

    Update Arduino_W5500_JSON_client.ino

--- examples/Arduino_W5500_JSON_client/Arduino_W5500_JSON_client.ino
+++ examples/Arduino_W5500_JSON_client/Arduino_W5500_JSON_client.ino
@@ -6,14 +6,13 @@
 /*|E-mail: martinius96@gmail.com                    |*/
 /*|Revízia: 1. Januar 2021                          |*/
 /*|-------------------------------------------------|*/
-//POZNAMKA: NEZABUDNITE PRI FYZICKOM MODULU S ARDUINOM PREPOJENI PRIPOJIT I INT (INTERRUPT) NA D2
 
 #include <Ethernet2.h>
 #include <ArduinoJson.h>
 const char* host = "192.168.1.XX"; //IP adresa WiFi termostatu
 const int httpPort = 80;
-byte mac[] = { 0xAA, 0xBB, 0xCC, 0x81, 0xBB, 0xAB };
-IPAddress ip(192, 168, 1, 253); //staticke pridelenie IP
+byte mac[] = { 0xAA, 0xBB, 0xCC, 0x81, 0xBB, 0xAC };
+IPAddress ip(192, 168, 1, 254); //staticke pridelenie IP
 EthernetClient client;
 unsigned long timer = 0;
 int interval = 15000;
@@ -24,9 +23,9 @@ void setup() {
     Ethernet.begin(mac); //nechame pridelenie na DHCP server
     // Ethernet.begin(mac, ip); //nastavime manualne
   }
-  Serial.print("  DHCP assigned IP ");
+  Serial.print(F("  DHCP assigned IP "));
   Serial.println(Ethernet.localIP());
-  Serial.println("Ready");
+  Serial.println(F("Ready"));
 }
 
 void loop() {
@@ -34,8 +33,8 @@ void loop() {
     timer = millis();
     client.stop();
     if (client.connect(host, httpPort)) {
-      String url = "/get_data.json";
-      Serial.println("Pripojenie uspesne, nacitavam JSON data");
+      String url = F("/get_data.json");
+      Serial.println(F("Pripojenie uspesne, nacitavam JSON data"));
       client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "User-Agent: W5500\r\n" + "Connection: close\r\n\r\n");
       while (client.connected()) {
         String line = client.readStringUntil('\n'); //HTTP HEADER
@@ -52,14 +51,14 @@ void loop() {
       float hystereza = obj[String("Hysteresis")];
       float cielova_teplota = obj[String("Target_Temperature")];
       float actual_temperature = obj[String("Actual_Temperature")];
-      Serial.print("Hystereza: ");
+      Serial.print(F("Hystereza: "));
       Serial.println(hystereza);
-      Serial.print("Cielova teplota: ");
+      Serial.print(F("Cielova teplota: "));
       Serial.println(cielova_teplota);
-      Serial.print("Namerana (aktualna) teplota: ");
+      Serial.print(F("Namerana (aktualna) teplota: "));
       Serial.println(actual_temperature);
     } else if (!client.connect(host, httpPort)) {
-      Serial.println("Nepodarilo sa pripojenie k termostatu, ani nacitanie JSON data");
+      Serial.println(F("Nepodarilo sa pripojenie k termostatu, ani nacitanie JSON data"));
     }
   }
 }
