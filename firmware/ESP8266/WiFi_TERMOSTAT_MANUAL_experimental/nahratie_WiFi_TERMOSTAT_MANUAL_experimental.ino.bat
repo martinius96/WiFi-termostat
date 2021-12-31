c7252ccbc9604ea8cb613330e98e9d57dbb6db2a
Martin Chlebovec <martinius96@gmail.com>
2021-12-31 14:56:20 +0100

    Update nahratie_WiFi_TERMOSTAT_MANUAL_experimental.ino.bat

--- firmware/ESP8266/WiFi_TERMOSTAT_MANUAL_experimental/nahratie_WiFi_TERMOSTAT_MANUAL_experimental.ino.bat
+++ firmware/ESP8266/WiFi_TERMOSTAT_MANUAL_experimental/nahratie_WiFi_TERMOSTAT_MANUAL_experimental.ino.bat
@@ -1,1 +1,1 @@
-start esptool.exe --port COM7 write_flash -fm dio 0x00000 WWiFi_TERMOSTAT_MANUAL_experimental.ino.bin
+start esptool.exe --port COM7 write_flash -fm dio 0x00000 WiFi_TERMOSTAT_MANUAL_experimental.ino.bin
